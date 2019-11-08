74f188c160ab56ca150e10074bafb81fc44a7871
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-08 03:06:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,1 +1,20 @@
+counter = 0
+  
+  while array[counter] do
+    return counter if array[counter] == value_to_find
+    counter += 1
+  end
+end
+ 
 
+def find_max_value(array)
+    
+    
+      counter = 0
+      max = 0
+    while array[counter] do
+      max = array[counter] if array[counter] > max
+        counter += 1
+    end
+    max
+end

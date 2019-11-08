21bc962c81259e0d16f2808b72d60f1bcb04cfad
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-08 02:23:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,3 @@
-  scale = [2,3,4,5]
-  scale_2 = [6,5,4,3]
-  hill = [1,2,3,4,5,4,3,2,1]
-  valley = [5,4,3,2,1,0,1,2,3,4,5]
-  zig_zag = [500,4,1000,5,250]
 
 def find_element_index(array, value_to_find)
  find_element_index.index(2)
@@ -16,3 +11,5 @@ def find_min_value(array)
   array.sort = new_array
   new_array.first 
 end
+
+

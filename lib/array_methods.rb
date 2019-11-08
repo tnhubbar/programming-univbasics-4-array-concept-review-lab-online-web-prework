2556c67710bf4bd384f45e03b8799ca25b00e231
Taryn Johnson <taryn.johnson919@gmail.com>
2019-11-08 02:23:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,3 +13,23 @@ def find_min_value(array)
 end
 
 
+counter = 0
+  
+  while array[counter] do
+    return counter if array[counter] == value_to_find
+    counter += 1
+  end
+end
+ 
+
+def find_max_value(array)
+    
+    
+      counter = 0
+      max = 0
+    while array[counter] do
+      max = array[counter] if array[counter] > max
+        counter += 1
+    end
+    max
+end
